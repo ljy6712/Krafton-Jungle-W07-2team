2d618638c6fd439f6d7a35a94a92f6ad2772db8c
Ubuntu <ubuntu@ip-172-31-8-3.ap-northeast-2.compute.internal>
2022-12-11 02:31:13 +0000

    echo server

--- echo/echoclient.c
+++ echo/echoclient.c
@@ -5,21 +5,21 @@ int main(int argc, char **argv){
     char *host, *port, buf[MAXLINE];
     rio_t rio;
 
-    if(argc != 3){
+    if(argc != 3){ // 받은 argc가 3개가 아니라면 잘못된 입력 (파일 경로, host, port)
         fprintf(stderr, "usage: %s <host> <port>\n", argv[0]);
-        exit(0);
+        exit(0); // 프로세스 종료
     }
     host = argv[1]; // 두번째 받은 값 host로 저장
     port = argv[2]; // 세번째 받은 값 port로 저장
 
-    clientfd = Open_clientfd(host, port);
+    clientfd = Open_clientfd(host, port); // 받은 host와 port에 연결 요청 듣고 있는 서버와 연결 설정 (반환값 IP 주소)
     Rio_readinitb(&rio, clientfd);
 
-    while (Fgets(buf, MAXLINE, stdin) != NULL){
+    while(Fgets(buf, MAXLINE, stdin) != NULL){
         Rio_writen(clientfd, buf, strlen(buf));
         Rio_readlineb(&rio, buf, MAXLINE);
         Fputs(buf, stdout);
     }
-    Close(clientfd);
-    exit(0);
+    Close(clientfd); // 열었던 모든 식별자들을 명시적으로 닫아주는 함수(불필요함)
+    exit(0); // 프로세스 종료
 }
--- echo/echoserveri.c
+++ echo/echoserveri.c
@@ -1,20 +1,30 @@
 #include "csapp.h"
 
-void echo(int connfd);
+void echo(int connfd){ // echo 함수
+    size_t n;
+    char buf[MAXLINE];
+    rio_t rio;
+
+    Rio_readinitb(&rio, connfd);
+    while((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0){
+        printf("server received %d bytes\n", (int)n);
+        Rio_writen(connfd, buf, n);
+    }
+}
 
 int main(int argc, char **argv){
     int listenfd, connfd;
     socklen_t clientlen;
-    struct sockaddr_storage clientaddr;     /* Enough space for any address */
+    struct sockaddr_storage clientaddr;     /* Enough space for any address */ // accecpt로 보내지는 소켓 주소 구조체
     char client_hostname[MAXLINE], client_port[MAXLINE];
 
-    if(argc != 2){ // 받은 argc가 2개가 아니라면 잘못된 입력
+    if(argc != 2){ // 받은 argc가 2개가 아니라면 잘못된 입력 (파일 경로, port)
         fprintf(stderr, "usage: %s <port>\n", argv[0]); // 에러 메시지 출력
-        exti(0); // 프로세스 종료
+        exit(0); // 프로세스 종료
     }
 
-    listenfd = Open_listenfd(argv[1]);
-    while(1){
+    listenfd = Open_listenfd(argv[1]); // 입력받은 port로 연결 요청 받을 준비
+    while(1){ // 무한루프
         clientlen = sizeof(struct sockaddr_storage);
         connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
         Getnameinfo((SA *) &clientaddr, clientlen, client_hostname, MAXLINE,
